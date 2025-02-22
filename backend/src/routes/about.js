46647f12ce731c6255b7e8dbe92dd7e823305f25
Yann <yann.perennes@epitech.eu>
2025-02-22 16:01:50 +0100

    fix(main): about.json route

--- backend/src/routes/about.js
+++ backend/src/routes/about.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const services = require('../config/servicesConfig');
 
-router.get('/about.json', (req, res) => {
+router.get('/', (req, res) => {
   const aboutInfo = {
     client: {
       host: req.ip
